ef84ebe4265899cef8ae01578b8b0b5f48a83987
Constantin <35843683+iFirebrand@users.noreply.github.com>
2024-12-07 11:15:51 -0500

    The base done before integration.

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -95,7 +95,7 @@ export const Header = () => {
           </div>
           <div className="flex flex-col">
             <span className="font-bold leading-tight">Ukrainian DC</span>
-            <span className="text-xs">Hub for community</span>
+            <span className="text-xs">For 🇺🇸 community</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
@@ -103,8 +103,8 @@ export const Header = () => {
         </ul>
       </div>
       <div className="navbar-end flex-grow mr-4">
-        <RainbowKitCustomConnectButton />
-        <FaucetButton />
+        {/* <RainbowKitCustomConnectButton />
+        <FaucetButton /> */}
       </div>
     </div>
   );
