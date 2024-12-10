3a99ea0b6b41462d3162977b74603ec580a5831c
Constantin <35843683+iFirebrand@users.noreply.github.com>
2024-12-10 09:23:18 -0500

    changes to camera time"

--- packages/nextjs/app/libs/[id]/scan.tsx
+++ packages/nextjs/app/libs/[id]/scan.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useCallback, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import Image from "next/image";
 import debounce from "lodash.debounce";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
