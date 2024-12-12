e556b2ac4b8b4af1b4e2d0a370e067ee1c4f2ea5
Constantin <35843683+iFirebrand@users.noreply.github.com>
2024-12-12 08:54:44 -0500

    text updates.

--- packages/nextjs/app/libs/[id]/LibraryClient.tsx
+++ packages/nextjs/app/libs/[id]/LibraryClient.tsx
@@ -49,29 +49,26 @@ export default function LibraryClient({ library }: LibraryClientProps) {
 
   return (
     <div className="flex flex-col items-center gap-y-5 pt-24 text-center">
-      <h1 className="text-2xl font-semibold">Page for {library.locationName} library</h1>
+      <h1 className="text-2xl font-semibold">Add to catalog at {library.locationName} library</h1>
       {isAtLibrary ? (
         <Scan libraryId={library.id} />
       ) : (
         <div className="flex justify-center">
           <div className="card bg-base-100 w-96 shadow-xl">
             <div className="card-body">
-              <h2 className="card-title">Must be at the library</h2>
-              <p>
-                Physical presence at the mini library is required. You need to be around 30 feet away to activate book
-                scanning feature.
-              </p>
+              <h2 className="card-title">You must be at the library to scan</h2>
+              <p>The scanning feature is activated when you are about 30 feet from the library.</p>
               <div className="card-actions justify-end">
                 <Link href="/" className="btn btn-primary">
-                  Go Home
+                  Back to Home
                 </Link>
               </div>
             </div>
             <div className="card-body">
               <h2 className="card-title">Or try troubleshooting</h2>
               <p>
-                Is precise location enabled? 📲 Settings {">"} General {">"} Privacy {">"} Location Services {">"}{" "}
-                Chrome or Safari {">"} Allow Location Access While Using App {">"} Precise location 😮‍💨
+                Is precise location enabled on your phone? 📲 Settings {">"} General {">"} Privacy {">"} Location
+                Services {">"} Chrome or Safari {">"} Allow Location Access While Using App {">"} Precise location 😮‍💨
               </p>
             </div>
           </div>
