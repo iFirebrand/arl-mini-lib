14cad2f1c118765d51613af6d70e00bb27c92c4a
Constantin <35843683+iFirebrand@users.noreply.github.com>
2024-12-13 14:14:23 -0500

    added google anlalytics

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "@rainbow-me/rainbowkit/styles.css";
 import "leaflet/dist/leaflet.css";
 import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
@@ -19,6 +20,17 @@ export const metadata = {
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <html suppressHydrationWarning>
+      <head>
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-205LFRGM0L" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-205LFRGM0L');
+          `}
+        </Script>
+      </head>
       <body>
         <ThemeProvider enableSystem>
           <ScaffoldEthAppWithProviders>{children}</ScaffoldEthAppWithProviders>
