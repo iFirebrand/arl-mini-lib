e9bc07b6c8c5d618e17f8d78e138edf2bbdc5ddb
Constantin <35843683+iFirebrand@users.noreply.github.com>
2024-12-13 11:29:32 -0500

    linting done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,1 @@
-export const metadata = {
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
+
--- packages/nextjs/app/browse/BrowseClient.tsx
+++ packages/nextjs/app/browse/BrowseClient.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import { InformationCircleIcon } from "@heroicons/react/24/outline";
 
